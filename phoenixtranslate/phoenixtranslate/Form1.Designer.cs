0617d4577ec24ff126c707f15e556f4fb7b568f7
paulomafio50 <Rackam50270>
2020-04-09 19:41:47 +0200

    broswer

--- phoenixtranslate/phoenixtranslate/Form1.Designer.cs
+++ phoenixtranslate/phoenixtranslate/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.geckoWebBrowser1 = new Gecko.GeckoWebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // geckoWebBrowser1
+            // 
+            this.geckoWebBrowser1.FrameEventsPropagateToMainWindow = false;
+            this.geckoWebBrowser1.Location = new System.Drawing.Point(61, 163);
+            this.geckoWebBrowser1.Name = "geckoWebBrowser1";
+            this.geckoWebBrowser1.Size = new System.Drawing.Size(602, 234);
+            this.geckoWebBrowser1.TabIndex = 0;
+            this.geckoWebBrowser1.UseHttpActivityObserver = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.geckoWebBrowser1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Gecko.GeckoWebBrowser geckoWebBrowser1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- phoenixtranslate/phoenixtranslate/Form1.cs
+++ phoenixtranslate/phoenixtranslate/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Gecko;
+using Gecko.DOM;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
 
 namespace phoenixtranslate
 {
@@ -15,6 +17,123 @@ namespace phoenixtranslate
         public Form1()
         {
             InitializeComponent();
+            Xpcom.Initialize("Firefox");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            geckoWebBrowser1.Navigate("https://www.deepl.com/translator");
+           
+        }
+        public string Extract(string xpath, string type)
+        {
+            string result = string.Empty;
+            GeckoHtmlElement Elm;
+            GeckoWebBrowser wb = geckoWebBrowser1;
+
+            if (wb != null)
+            {
+                Elm = GetElement(wb, xpath);
+
+            if (Elm != null)
+            {
+                if (Elm != null)
+                {
+                    switch (type)
+                    {
+                        case "html":
+                            {
+                                result = Elm.OuterHtml;
+                                break;
+                            }
+
+                        case "text":
+                            {
+                                if (Elm.GetType().Name == "GeckoTextAreaElement")
+                                    result = ((GeckoTextAreaElement)Elm).Value;
+                                else
+                                    result = Elm.TextContent.Trim();
+                                break;
+                            }
+
+                        case "value":
+                            {
+                                result = ((GeckoInputElement)Elm).Value;
+                                break;
+                            }
+
+                        default:
+                            {
+                                result = ExtractData(Elm, type);
+                                break;
+                            }
+                    }
+                }
+            }
+        }
+
+        return result;
+        }
+        private string ExtractData(GeckoHtmlElement ele, string attribute)
+        {
+            var result = string.Empty;
+
+            if (ele != null)
+            {
+                var tmp = ele.GetAttribute(attribute);
+                if (tmp != null)
+                    result = tmp.Trim();
+            }
+
+            return result;
+        }
+
+        private GeckoHtmlElement GetElement(GeckoWebBrowser wb, string xpath)
+        {
+            GeckoHtmlElement elm = null/* TODO Change to default(_) if this is not a reference type */;
+
+            if (xpath.StartsWith("/"))
+            {
+                if (xpath.Contains("@class") || xpath.Contains("@data-type"))
+                {
+                    var html = GetHtmlFromGeckoDocument(wb.Document);
+                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                    doc.LoadHtml(html);
+                    var node = doc.DocumentNode.SelectSingleNode(xpath);
+
+                    if (node != null)
+                    {
+                        var currentXpath = "/" + node.XPath;
+                        elm = (GeckoHtmlElement)wb.Document.EvaluateXPath(currentXpath).GetNodes().FirstOrDefault();
+                    }
+                }
+                else
+                    elm = (GeckoHtmlElement)wb.Document.EvaluateXPath(xpath).GetNodes().FirstOrDefault();
+            }
+            else
+                elm = (GeckoHtmlElement)wb.Document.GetElementById(xpath);
+
+            return elm;
+        }
+
+        private string GetHtmlFromGeckoDocument(GeckoDocument doc)
+        {
+            var result = string.Empty;
+            GeckoHtmlElement element;
+            var geckoDomElement = doc.DocumentElement;
+
+            if (geckoDomElement is GeckoHtmlElement)
+            {
+                element = (GeckoHtmlElement)geckoDomElement;
+                result = element.InnerHtml;
+            }
+
+            return result;
         }
     }
 }
