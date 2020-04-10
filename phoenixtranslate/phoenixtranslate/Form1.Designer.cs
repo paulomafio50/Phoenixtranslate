59e36bb7081c277a909fd98dbcc684556dbd0fef
paulomafio50 <Rackam50270>
2020-04-10 13:47:14 +0200

    import fonctionnel

--- phoenixtranslate/phoenixtranslate/Form1.Designer.cs
+++ phoenixtranslate/phoenixtranslate/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // geckoWebBrowser1
@@ -71,11 +72,20 @@
             this.textBox1.Size = new System.Drawing.Size(301, 85);
             this.textBox1.TabIndex = 3;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(761, 599);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(230, 85);
+            this.textBox2.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1209, 710);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -94,6 +104,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- phoenixtranslate/phoenixtranslate/Form1.cs
+++ phoenixtranslate/phoenixtranslate/Form1.cs
@@ -9,6 +9,7 @@ using Gecko;
 using Gecko.DOM;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
+using System.Text;
 
 namespace phoenixtranslate
 {
@@ -22,12 +23,12 @@ namespace phoenixtranslate
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            geckoWebBrowser1.Navigate("https://www.deepl.com/translator");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            geckoWebBrowser1.Navigate("https://www.deepl.com/translator");
+            
          
         }
         public string Extract(string xpath, string type)
@@ -96,7 +97,7 @@ namespace phoenixtranslate
         private GeckoHtmlElement GetElement(GeckoWebBrowser wb, string xpath)
         {
 
-            GeckoHtmlElement elm = null/* TODO Change to default(_) if this is not a reference type */;
+            GeckoHtmlElement elm = null;
 
             if (xpath.StartsWith("/"))
             {
@@ -139,11 +140,58 @@ namespace phoenixtranslate
 
         private void button2_Click(object sender, EventArgs e)
         {
-           
-                //GeckoTextAreaElement inputter = (GeckoTextAreaElement)geckoWebBrowser1.Document.G
-           
 
-            textBox1.Text = Extract("/html[1]/body[1]/div[2]/div[1]/div[1]/div[4]/div[3]/div[1]/textarea[1]", "text");
+         
+            fill("/html[1]/body[1]/div[2]/div[1]/div[1]/div[3]/div[2]/div[1]/textarea[1]", textBox1.Text);
+            wait(5000);//a changer
+          
+          textBox2.Text = Extract("/html[1]/body[1]/div[2]/div[1]/div[1]/div[4]/div[3]/div[1]/textarea[1]", "text");
+        }
+        public void fill(string xpath, string value)
+        {
+            GeckoWebBrowser wb = geckoWebBrowser1;
+            if (wb != null)
+            {
+                if (xpath.StartsWith("/"))
+                {
+                    GeckoHtmlElement elm = GetElement(wb, xpath);
+                    if (elm != null)
+                    {
+               
+                  
+                                GeckoTextAreaElement input1 = (GeckoTextAreaElement)elm;
+                                input1.Value = "";
+                                input1.Focus();
+                                input1.Click();
+                                Clipboard.SetText(value);
+                                geckoWebBrowser1.Paste();
+                     
+                    }
+                
+                
+                }
+            }
+
+        }
+
+        public void wait(int milliseconds)
+        {
+            System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+            //Console.WriteLine("start wait timer");
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                //Console.WriteLine("stop wait timer");
+            };
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
         }
     }
 }
